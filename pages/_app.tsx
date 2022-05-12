c0a301f6250587c5040208b7b700f55a786e6474
Naoki Hirayama <naoki.hirayama1987@gmail.com>
2022-05-12 22:51:11 +0900

    no message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
-export default function MyApp({ Component, pageProps }) {
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/plans/[title].tsx
+++ pages/plans/[title].tsx
@@ -1,7 +1,15 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Title() {
   const router = useRouter();
-  console.log(router.query);
-  return <h1>{router.query.title}タイトルppdcdcdcd</h1>;
+  return (
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+        <div className="flex justify-between items-end gap-4 mb-6">
+          <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold">{router.query.title}</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
